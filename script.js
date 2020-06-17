e19770b25f2fed50d518caf4e01f478f1a5bf73a
Sinoptik93 <chepukhin.sergey@gmail.com>
2020-06-17 23:47:50 +0500

    add dynamic slider style function | step 3

---
+++ script.js
@@ -0,0 +1,22 @@
+// REFACTOR THIS
+const depositeRangeElem = document.getElementsByClassName('deposite-range_result-value')[0];
+const depositeResultAreaElem = document.getElementsByClassName('deposite-range_result-area')[0];
+
+// Refactor this
+const replAmountRangeElem = document.getElementsByClassName('replenishment-amount_result-value')[0];
+const replAmountResultAreaElem = document.getElementsByClassName('replenishment-amount_result-area')[0];
+
+const renderResultArea = (areaElement, sliderElement) => {
+  sliderElement.oninput = () => {
+    areaElement.innerText = Number(sliderElement.value).toLocaleString('ru-RU');
+  };
+  sliderElement.addEventListener('mousemove', () => {
+    const currentValue = sliderElement.value;
+    const currentFillPercent = ((currentValue * 100) / sliderElement.attributes.max.value).toFixed(0);
+    const currentFillBackground = `linear-gradient(90deg, #a3ca4c ${currentFillPercent}%, #1e1f22 ${currentFillPercent}%)`;
+    sliderElement.style.background = currentFillBackground;
+  });
+};
+
+renderResultArea(depositeResultAreaElem, depositeRangeElem);
+renderResultArea(replAmountResultAreaElem, replAmountRangeElem);
